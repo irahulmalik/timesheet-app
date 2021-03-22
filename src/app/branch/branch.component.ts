97ddb661aaf8e8c0ffc941616a861f8bb9576ca9
irahulmalik <rahulmalik1995@gmail.com>
2021-03-22 10:32:05 +0530

    making apply for leave page

--- src/app/branch/branch.component.ts
+++ src/app/branch/branch.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {FormGroup, FormControl} from '@angular/forms';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-branch',
   templateUrl: './branch.component.html',
@@ -14,15 +15,32 @@ export class BranchComponent implements OnInit {
   });
   minDate: Date;
   maxDate: Date;
-  constructor() { }
+  constructor(private auth: AuthService) { }
   
 
   ngOnInit(): void {}
 leave(){
-  console.log("Leave called", this.range.value)
+  let startd = this.range.value.start.toISOString().split("T")
+  console.log("Leave called", startd[0])
   console.log("iterating for date")
   // for( let i: Date = this.range.value.start ; i<=this.range.value.end ; i.getDate() +1){
     // console.log(i)
   // } 
+  let loop = this.range.value.start
+  while(loop.toISOString()<=this.range.value.end.toISOString()){
+    console.log("suo",loop.toISOString())
+    let newDate= loop.setDate(loop.getDate() +1)
+    loop = new Date(newDate)
+    let mydata = {
+      taskname:"leave",
+      leave :"true",
+      duration:"",
+      won:"",
+      taskcategory:"",
+      date: loop.toISOString()
+    }
+    console.log(mydata)
+    // this.auth.addtask(mydata)
+  }
 }
 }
